6de246f660502ccba59b0ca98fd0d4737a1eb6db
Bryan Fink <bryan@basho.com>
2010-12-13 16:10:02 -0500

    document what each verifier does

--- src/mapred_verifiers.erl
+++ src/mapred_verifiers.erl
@@ -2,14 +2,27 @@
 
 -compile([export_all]).
 
+%% @type type() = bucket|entries
+
+%% @spec map(type(), [term()], integer()) -> boolean()
+%% @doc Checks that the Result is TotalEntries in length.
+%%      Returns 'true' if so, false if not.
 map(_Type, Result, TotalEntries) ->
     io:format("Got ~p, Expecting: ~p...", [length(Result), TotalEntries]),
     length(Result) == TotalEntries.
 
+%% @spec simple(type(), [term()], integer()) -> boolean()
+%% @doc Checks that Result is a single-element list, that element
+%%      being an integer less than or equal to TotalEntries.
+%%      TODO: actually not checking 'integer', just =<
 simple(_Type, Result, TotalEntries) ->
     [R] = Result,
     R =< TotalEntries.
 
+%% @spec sorted(type(), [term()], integer()) -> boolean()
+%% @doc Checks that Result is a list whose length is less than or
+%%      equal to TotalEntries, and whose elements are sorted.
+%%      TODO: only checking first two elements of list, not the whole thing
 sorted(_Type, Result, TotalEntries) ->
     RS = length(Result),
     case RS == TotalEntries orelse TotalEntries > RS of
